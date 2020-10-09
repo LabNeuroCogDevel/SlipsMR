b75ca52bab1d4fe456b7faf486f9697572392a20
WillForan <willforan@gmail.com>
2020-10-08 22:16:25 -0400

    🔥 mktiming: rework for block design

--- mktiming.R
+++ mktiming.R
@@ -36,11 +36,11 @@ mktime <- function(d, pfix) {
    }
 
    # all one block. set side deval and if response was correct
-   # TODO: does no response make it throug filter??
-   #   make make all NA -> False?
+   # ID_{start,end} should already have blocks set
+   if(! "block" %in% names(d)) d$block <- 1
+
    d <- d %>%
-      mutate(block=1,
-             disp_side=substr(LR1, 0, 1),
+      mutate(disp_side=substr(LR1, 0, 1),
              deval=deval=='True',
              resp_cor=ifelse(deval, score_raw > -1, score_raw >= 1))
 
@@ -101,19 +101,21 @@ mk1d_from_folder <- function(infolder){
      dir.create(outfolder,recursive=T)
 
   findcsv(infolder, 'OD')  %>% mktime(pfix=paste0(outfolder,'OD'))
-  findcsv(infolder, 'SOA') %>% mktime(pfix=paste0(outfolder,'SOA'))
-  findcsv(infolder, 'DD')  %>% mktime(pfix=paste0(outfolder,'DD'))
+  findcsv(infolder, 'SOA')  %>%
+      fix_bad_onsets %>%
+      mktime(pfix=paste0(outfolder,'SOA'))
+  findcsv(infolder, 'DD')  %>%
+      fix_bad_onsets %>%
+      mktime(pfix=paste0(outfolder,'DD'))
 
-  # ID is special. we want to break up for "learning" contrasts
-  idphase <- findcsv(infolder, 'ID') %>%
-     mutate(score_block = 1+lag(cumsum(ttype=='TrialType.SCORE'),default=0),
-            block_name  = cut(score_block,
-                              c(0,2,4,6,8),
-                              c("1-2","3-4","5-6","7-8")))
-  idphase %>% mktime(pfix=paste0(outfolder,'ID'))
-
-
-
+  ## ID is special. we want to break up for "learning" contrasts
+  # # 20201008 - OLD if we had ID as one long run
+  # idphase <- findcsv(infolder, 'ID_[0-9]') %>%
+  #    mutate(score_block = 1+lag(cumsum(ttype=='TrialType.SCORE'),default=0),
+  #           block_name  = cut(score_block,
+  #                             c(0,2,4,6,8),
+  #                             c("1-2","3-4","5-6","7-8")))
+  # idphase %>% mktime(pfix=paste0(outfolder,'ID'))
   # > idphase %>% select(score_block,block_name) %>% distinct
   # score_block block_name
   #           1        1-2
@@ -124,6 +126,17 @@ mk1d_from_folder <- function(infolder){
   #           6        5-6
   #           7        7-8
   #           8        7-8
+
+  # 20201008 - truncated ID in two sets. merge back together
+  idphase_start <- findcsv(infolder, 'ID_start') %>%
+     mutate(block=1, block_name  = "start")
+  idphase_end <- findcsv(infolder, 'ID_end') %>%
+     mutate(block=2, block_name  = "end")
+  idphase <- rbind(idphase_start, idphase_end)
+  idphase %>% mktime(pfix=paste0(outfolder,'ID'))
+
+
+
   for(bn in unique(idphase$block_name)) {
      blockedfolder <- paste(sep="/", outfolder, 'IDblks', bn)
      if(!dir.exists(blockedfolder)) dir.create(blockedfolder, recursive=T)
@@ -131,6 +144,42 @@ mk1d_from_folder <- function(infolder){
      mktime(idphase %>% filter(block_name == bn),
             pfix=paste0(blockedfolder,'/'))
   }
+
+  # block format for SOA and DD
+  # TODO!!
+  blkdir <- paste(sep="/", outfolder, 'blks')
+  if(!dir.exists(blkdir)) dir.create(blkdir, recursive=T)
+
+  cat("# writting SOA.1d and DD.1d to", blkdir,"\n")
+  soa_blk <- findcsv(infolder, 'SOA') %>% to_block %>%
+      LNCDR::save1D('onset', file.path(blkdir,"SOA.1d"), dur='dur', nblocks=1)
+  dd_blk <- findcsv(infolder, 'DD')  %>% to_block %>%
+      LNCDR::save1D('onset', file.path(blkdir,"DD.1d"), dur='dur', nblocks=1)
+}
+
+fix_bad_onsets <- function(soadd) {
+    # pilot did not set onset time for GRID and SCORE (it's negative starttime instaed)
+    bad_grid <- which(grepl("GRID", soadd$ttype) & soadd$onset < 0)
+    soadd$onset[bad_grid] <- soadd$onset[bad_grid +1] - 5
+    bad_score <- which(grepl("SCORE", soadd$ttype) & soadd$onset < 0)
+    soadd$onset[bad_score] <- soadd$onset[bad_score -1] +
+        soadd$rt_raw[bad_score -1]
+    return(soadd)
+}
+
+to_block <- function(d_all, iti_time=40) {
+  d <- filter(d_all, grepl("SHOW",ttype))
+  start_idx <- which(c(Inf,diff(d$onset)) >= iti_time)
+  end_idx <- c(start_idx[-1] - 1, nrow(d))
+  # N.B. group_by(extra) doesn't work because repeated. 2x {DD|SOA}_2 and {DD|SOA}_4
+  blk_df <- d[start_idx, c('extra', 'onset')] %>%
+      mutate(onset = onset,
+             end = d$onset[end_idx],
+             last_tr = d$rt_raw[end_idx],
+             dur = end - onset + last_tr,
+             n = 1:n(),
+             block=1)
+
 }
 
 ## do it for pilot
